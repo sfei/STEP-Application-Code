9427d34b86c84220bfdb49a535924cffeaa5fa54
Lawrence <lawrences@sfei.org>
2018-06-21 12:32:14 -0700

    Fade effect

--- js/init-storymap.js
+++ js/init-storymap.js
@@ -52,6 +52,7 @@ function init(root, config) {
         'models/app-step', 
         'models/app-scene'
     ], function(ieFixes, domReady, STEP, Scene) {
+        
         // init STEP
         root.step = new STEP({mapserverUrl: config.mapserverUrl});
         root.step.init({
@@ -64,6 +65,9 @@ function init(root, config) {
                 endYear: 2014
             }
         });
+        // turn off no-data points
+        root.step.modules.queryAndUI.toggleNoDataDisplay(false, true);
+        
         // legend show/hide modified since we put tab on top of screen now
         root.step.modules.legend.legendHide = function() {
             $("#legend-container").hide();
@@ -74,8 +78,7 @@ function init(root, config) {
             $("#legend-container").show();
             $("#show-legend-tab").hide("slide", { direction: "up" }, 100);
         };
-        // turn off no-data points
-        root.step.modules.queryAndUI.toggleNoDataDisplay(false, true);
+        
         // init storymap scene
         root.scene = new Scene({
             container:         "#storymap-container", 
@@ -85,16 +88,43 @@ function init(root, config) {
             offset:            0, 
             resizeHeightElems: ".sm-page, .sm-spacer, .sm-mobile-spacer"
         });
-        // add actions
-        var currentView = {
-            center: root.step.map.getView().getCenter(), 
-            zoom: root.step.map.getView().getZoom()
-        };
-        var changeFrame = function(frameNo) {
+        
+        // change frame action
+        var changeFrame = function(frameNo, reverse) {
+            frameNo = parseInt(frameNo);
             var isMap = frameNo === 4;
-            $(".frame").hide();
-            $("#frame-"+frameNo).show();
-            if(isMap) root.step.map.updateSize();
+            if(isMap) {
+                $(".frame").hide();
+                $("#frame-"+frameNo).show();
+                root.step.map.updateSize();
+            } else if(frameNo === 1 && !reverse) {
+                $(".frame").hide();
+                $("#frame-"+frameNo).show();
+            } else {
+                var prevFrame = $("#frame-"+(frameNo + (reverse ? 1 : -1))).css({
+                                    'position': "absolute", 
+                                    'top': 0, 
+                                    'left': 0, 
+                                    'opacity': 1, 
+                                    'z-index': 99
+                                });
+                var nextFrame = $("#frame-"+frameNo).css({
+                                    'position': "absolute", 
+                                    'top': 0, 
+                                    'left': 0, 
+                                    'opacity': 0, 
+                                    'z-index': 90
+                                }).show();
+                var clear = {'opacity':'','z-index':'','position':'','top':'','left':''};
+                prevFrame.animate(
+                    {opacity: 0}, 300, 'swing', 
+                    function() { prevFrame.hide().css(clear); }
+                );
+                nextFrame.animate(
+                    {opacity: 1}, 300, 'swing', 
+                    function() { nextFrame.show().css(clear); }
+                );
+            }
         };
         root.scene.addAction(
             "swapFrame", 
@@ -102,28 +132,37 @@ function init(root, config) {
                 changeFrame(elem.getAttribute("next-frame"));
             }, 
             function(elem) {
-                changeFrame(parseInt(elem.getAttribute("next-frame"))-1);
+                changeFrame(parseInt(elem.getAttribute("next-frame"))-1, true);
             }
         );
-        root.scene.addAction(
-            "mapFunctionA", 
-            function() {
-                root.step.map.getView().animate({
-                    center: [-12796399.011894481, 3947754.771558293], 
-                    zoom: 8
-                });
-                root.step.modules.legend.legendHide();
-            }, 
-            function() {
-                root.step.map.getView().animate(currentView);
-                root.step.modules.legend.legendShow();
-            }
-        );
+        
+        // map zoom action
+//        var currentView = {
+//            center: root.step.map.getView().getCenter(), 
+//            zoom: root.step.map.getView().getZoom()
+//        };
+//        root.scene.addAction(
+//            "mapFunctionA", 
+//            function() {
+//                root.step.map.getView().animate({
+//                    center: [-12796399.011894481, 3947754.771558293], 
+//                    zoom: 8
+//                });
+//                root.step.modules.legend.legendHide();
+//            }, 
+//            function() {
+//                root.step.map.getView().animate(currentView);
+//                root.step.modules.legend.legendShow();
+//            }
+//        );
+
         // ensure resizing of step map (as it doesn't when offscreen)
         window.addEventListener('resize', function() { root.step.map.updateSize(); });
+        
         // turn on first frame
         $(".frame").hide();
         $("#frame-1").show();
+        
         // fade out loading
         var loadingDiv = document.querySelector("#storymap-loading");
         loadingDiv.innerHTML = "";
@@ -132,6 +171,7 @@ function init(root, config) {
             loadingDiv.remove();
             document.body.style.removeProperty("overflow-y");
         }, 800);
+        
     }, function(e) {
         console.log(e);
         alert("There was an error initializing the required libraries.");
